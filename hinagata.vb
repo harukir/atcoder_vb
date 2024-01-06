149f5a11def7e3ebeeacf383ccfd07ab0c942277
harukir <harukir@gmail.com>
2024-01-06 14:18:10 +0900

    二分探索追加

--- hinagata.vb
+++ hinagata.vb
@@ -27,7 +27,6 @@
 
 
 
-
 '#Region "ライブラリ"
 
 '#Region "union find"
@@ -35,7 +34,10 @@
 
 '        'union by rank未実装
 
+'        ''' <summary>親リスト</summary>
 '        Public par As List(Of Decimal)
+
+'        ''' <summary>深さ(union by rank未実装なので、現状意味のないリスト)</summary>
 '        Public rank As List(Of Decimal)
 
 '        Sub New(ByVal itemCount As Decimal)
@@ -141,6 +143,49 @@
 '    End Sub
 
 '    ''' <summary>
+'    ''' 二分探索
+'    ''' </summary>
+'    ''' <param name="ary"></param>
+'    ''' <param name="key"></param>
+'    ''' <returns>
+'    ''' aryの中からkeyの値で二分探索を行う。
+'    ''' 戻り値はReturn部のコメント参照
+'    ''' 参考URL：https://qiita.com/drken/items/97e37dd6143e33a64c8c
+'    ''' </returns>
+'    Function binarySearch(ary As Decimal(), key As Decimal) As Decimal
+
+'        Dim L As Decimal = -1 '「index = 0」が条件を満たすこともあるので、初期値は -1
+'        Dim R As Decimal = ary.Count '「index = a.size()-1」が条件を満たさないこともあるので、初期値は a.size()
+
+'        ' どんな二分探索でもここの書き方を変えずにできる！
+'        While (R - L > 1)
+
+'            Dim M As Decimal = Math.Truncate(L + (R - L) / 2)
+
+'            If ary(M) >= key Then
+'                R = M
+'            Else
+'                L = M
+'            End If
+'        End While
+
+'        'L は「key未満(key含まない)の内、最大の値(リストに該当値が無ければ-1)」、R は「key以上(key含む)の内、最小の値(リストに該当値が無ければ-1)」となる
+'        'Return L
+'        Return If(R <= ary.Count - 1, R, -1)
+
+'    End Function
+'    ''' <summary>
+'    ''' 二分探索
+'    ''' </summary>
+'    ''' <param name="ary"></param>
+'    ''' <param name="key"></param>
+'    ''' <returns></returns>
+'    Function binarySearch(ary As List(Of Decimal), key As Decimal) As Decimal
+'        Return binarySearch(ary.ToArray, key)
+'    End Function
+
+
+'    ''' <summary>
 '    ''' BFS
 '    ''' サンプルとしてABC309D(https://atcoder.jp/contests/abc309/tasks/abc309_d)で使用したBFSである「引数のグラフの中で、startPから最も遠い距離を返すBFS」を実装
 '    ''' </summary>
@@ -200,6 +245,4 @@
 '#End Region
 
 'End Module
-
-
 
